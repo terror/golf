cab3d60ff9b6f913099e9f6df19aa96145047a24
Liam <liam@scalzulli.com>
2021-06-07 19:08:26 -0400

    Make `shoppinglisteasy` absurd

--- src/shoppinglisteasy.py
+++ src/shoppinglisteasy.py
@@ -1,9 +1,8 @@
 # https://open.kattis.com/problems/shoppinglisteasy
+# this is absurd
+# is there a better way to do this? probably
 
-import collections
+import sys, collections, functools
 
-def main(n, m, d):
-    for _ in range(n): d += collections.Counter(list(map(str, input().split())))
-    print(*(lambda ans: (len(ans), *sorted(ans)))([k for k in d if d[k] == n]), sep="\n")
-
-if __name__ == "__main__": main(*map(int, input().split()), collections.Counter())
+def main(n, m, d): print(*(lambda ans: (len(ans), *sorted(ans)))([k for k in d if d[k] == n]), sep="\n")
+if __name__ == "__main__": main(*map(int, input().split()), (lambda l: functools.reduce(lambda x, y: x + y, [collections.Counter()] + [collections.Counter(o.split()) for o in l]))(sys.stdin.readlines()))
