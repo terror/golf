9d38f8376c2ddbbb34360a3353ec6f4289736bd4
Liam <liam@scalzulli.com>
2021-06-13 13:16:34 -0400

    Refactor a few solutions

--- src/metaprogramming.py
+++ src/metaprogramming.py
@@ -2,12 +2,12 @@
 
 import sys
 
+eval = lambda a, b, c, d: {'>': lambda a, b: d[a] > d[b], '<': lambda a, b: d[a] < d[b], '=': lambda a, b: d[a] == d[b]}[b](a, c)
+
 def main(d):
   for i in sys.stdin:
     q, *x = list(map(str, i.split()))
     if q == 'define': d[x[1]] = int(x[0])
-    if q == 'eval': print('undefined' if not all(k in d for k in (x[0], x[2])) else ('true' if eval(x, d) else 'false'))
-
-def eval(i, d) -> bool: a, b, c = i; return {'>': lambda a, b: d[a] > d[b], '<': lambda a, b: d[a] < d[b], '=': lambda a, b: d[a] == d[b]}[b](a, c)
+    if q == 'eval': print('undefined' if not all(k in d for k in (x[0], x[2])) else ('true' if eval(*x, d) else 'false'))
 
 if __name__ == '__main__': main({})
--- src/shoppinglisteasy.py
+++ src/shoppinglisteasy.py
@@ -1,8 +1,5 @@
 # https://open.kattis.com/problems/shoppinglisteasy
-# this is absurd
-# is there a better way to do this? probably
 
 import sys, collections, functools
 
-def main(n, m, d): print(*(lambda ans: (len(ans), *sorted(ans)))([k for k in d if d[k] == n]), sep="\n")
-if __name__ == "__main__": main(*map(int, input().split()), (lambda l: functools.reduce(lambda x, y: x + y, [collections.Counter()] + [collections.Counter(o.split()) for o in l]))(sys.stdin.readlines()))
+(lambda n, m, d: print(*(lambda ans: (len(ans), *sorted(ans)))([k for k in d if d[k] == n]), sep="\n"))(*map(int, input().split()), (lambda l: functools.reduce(lambda x, y: x + y, [collections.Counter()] + [collections.Counter(o.split()) for o in l]))(sys.stdin.readlines()))
